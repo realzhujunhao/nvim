ba60fa5fc1f96f432d00b0f79d3229fd51823585
Junhao Zhu <realzhujunhao@gmail.com>
2025-03-07 14:23:18 +0800

    disable gitignore filter nvimtree

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -20,6 +20,16 @@ return {
                 },
             },
         },
+        filters = {
+            enable = true,
+            git_ignored = false,
+            dotfiles = false,
+            git_clean = false,
+            no_buffer = false,
+            no_bookmark = false,
+            custom = {},
+            exclude = {},
+        },
     },
     lazy = false,
     init = function()
